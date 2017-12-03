4455e07f8b1f0b58d74f5cfb690e5d62693d6911
Will Carty <cartycrabber@gmail.com>
2017-12-02 22:42:01 -0500

    Add button to new rat screen that autofills info from location

--- app/src/main/java/com/github/muffindreamers/rous/controllers/AddNewRatData.java
+++ app/src/main/java/com/github/muffindreamers/rous/controllers/AddNewRatData.java
@@ -1,10 +1,17 @@
 package com.github.muffindreamers.rous.controllers;
 
+import android.Manifest;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.location.Address;
+import android.location.Geocoder;
+import android.location.Location;
 import android.os.AsyncTask;
 import android.os.Build;
 import android.os.Bundle;
 import android.support.annotation.RequiresApi;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.text.Editable;
 import android.util.Log;
@@ -18,7 +25,12 @@ import com.github.muffindreamers.rous.R;
 import com.github.muffindreamers.rous.model.Borough;
 import com.github.muffindreamers.rous.model.RatData;
 import com.github.muffindreamers.rous.model.UploadRatData;
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.tasks.OnSuccessListener;
 
+import java.io.IOException;
+import java.util.List;
 import java.util.concurrent.ExecutionException;
 
 
@@ -26,13 +38,15 @@ import java.util.concurrent.ExecutionException;
  * Created by Brooke 10/1/17
  */
 public class AddNewRatData extends AppCompatActivity {
-    private Spinner locationType;
-    private Spinner borough;
-    private EditText zipCode;
-    private EditText city;
-    private EditText address;
-    private EditText latitude;
-    private EditText longitude;
+    private Spinner locationTypeSpinner;
+    private Spinner boroughSpinner;
+    private EditText zipCodeText;
+    private EditText cityText;
+    private EditText addressText;
+    private EditText latitudeText;
+    private EditText longitudeText;
+
+    private FusedLocationProviderClient locationClient;
 
     /**
      * Creates and takes in new rat data
@@ -44,31 +58,91 @@ public class AddNewRatData extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_new_rat_data);
 
-        locationType = (Spinner) findViewById(R.id.location_type_spinner);
-        borough = (Spinner) findViewById(R.id.borough_edit);
-        zipCode = (EditText) findViewById(R.id.zipCode_edit);
-        city = (EditText) findViewById(R.id.city_edit);
-        address = (EditText) findViewById(R.id.address_edit);
-        latitude = (EditText) findViewById(R.id.latitude_edit);
-        longitude = (EditText) findViewById(R.id.longitude_edit);
+        locationTypeSpinner = (Spinner) findViewById(R.id.location_type_spinner);
+        boroughSpinner = (Spinner) findViewById(R.id.borough_edit);
+        zipCodeText = (EditText) findViewById(R.id.zipCode_edit);
+        cityText = (EditText) findViewById(R.id.city_edit);
+        addressText = (EditText) findViewById(R.id.address_edit);
+        latitudeText = (EditText) findViewById(R.id.latitude_edit);
+        longitudeText = (EditText) findViewById(R.id.longitude_edit);
 
 
         ArrayAdapter<String> adapter = new ArrayAdapter<>(this,
                 android.R.layout.simple_spinner_dropdown_item, RatData.getLocationTypeArray());
         adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-        locationType.setAdapter(adapter);
+        locationTypeSpinner.setAdapter(adapter);
 
         ArrayAdapter<Borough> boroughAdapter =
                 new ArrayAdapter<>(this,android.R.layout.simple_spinner_item, Borough.values());
         boroughAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-        borough.setAdapter(boroughAdapter);
+        boroughSpinner.setAdapter(boroughAdapter);
 
         Button cancel = (Button) findViewById(R.id.cancel_add_rat);
         cancel.setOnClickListener(this::cancelHandler);
 
         Button add_new_new = (Button) findViewById(R.id.add_new_new);
         add_new_new.setOnClickListener(this::newRatDataHandler);
+
+        ((Button) findViewById(R.id.use_location)).setOnClickListener(this::useLocationHandler);
+
+        locationClient = LocationServices.getFusedLocationProviderClient(this);
     }
+
+    /**
+     * Handles the user pressing the Use Location button
+     * @param v the view the button is located in
+     */
+    private void useLocationHandler(View v) {
+        autofillFromLocation();
+    }
+
+    /**
+     * Automatically fills in textviews using the devices current location
+     */
+    private void autofillFromLocation() {
+        if(ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION)
+                != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this, new String[] {Manifest.permission.ACCESS_FINE_LOCATION},
+                    1);
+            return;
+        }
+
+        Geocoder geocoder = new Geocoder(this);
+        locationClient.getLastLocation().addOnSuccessListener(this, new OnSuccessListener<Location>() {
+            @Override
+            public void onSuccess(Location location) {
+                if(location != null) {
+                    latitudeText.setText("" + location.getLatitude());
+                    longitudeText.setText("" + location.getLongitude());
+
+                    try {
+                        List<Address> addresses = geocoder.getFromLocation(location.getLatitude(), location.getLongitude(), 1);
+
+                        if(addresses != null && addresses.size() > 0) {
+                            Address address = addresses.get(0);
+                            zipCodeText.setText(address.getPostalCode());
+                            cityText.setText(address.getLocality());
+                            addressText.setText(address.getSubThoroughfare() + " " + address.getThoroughfare());
+                        }
+                    } catch (IOException e) {
+                        throw new RuntimeException(e);
+                    }
+                }
+            }
+        });
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String permissions[], int[] grantResults) {
+        switch (requestCode) {
+            case 1: //Access to fine location
+                if(grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    autofillFromLocation();
+                }
+                return;
+        }
+    }
+
     /**
      * Returns the user to the Main Screen
      * @param v the view the button is located in
@@ -95,19 +169,19 @@ public class AddNewRatData extends AppCompatActivity {
         RatData addedRat = new RatData();
 
         try {
-            Object locationItem = locationType.getSelectedItem();
+            Object locationItem = locationTypeSpinner.getSelectedItem();
             addedRat.setLocationType(locationItem.toString());
-            Object boroughItem = borough.getSelectedItem();
+            Object boroughItem = boroughSpinner.getSelectedItem();
             addedRat.setBorough(boroughItem.toString());
-            Editable zipCodeItem = zipCode.getText();
+            Editable zipCodeItem = zipCodeText.getText();
             addedRat.setZipCode((Integer.parseInt(zipCodeItem.toString())));
-            Editable cityItem = city.getText();
+            Editable cityItem = cityText.getText();
             addedRat.setCity(cityItem.toString());
-            Editable addressItem = address.getText();
+            Editable addressItem = addressText.getText();
             addedRat.setStreetAddress(addressItem.toString());
-            Editable latitudeItem = latitude.getText();
+            Editable latitudeItem = latitudeText.getText();
             addedRat.setLatitude((Double.parseDouble(latitudeItem.toString())));
-            Editable longitudeItem = longitude.getText();
+            Editable longitudeItem = longitudeText.getText();
             addedRat.setLongitude((Double.parseDouble(longitudeItem.toString())));
         } catch(RuntimeException ex) {
             Log.e("AddNewRatData", "Parse error", ex);
